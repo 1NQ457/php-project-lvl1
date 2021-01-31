6084f53a70347dfe620041f7ad8b68d961a5148e
1NQ457 <alex.kot14@gmail.com>
2021-01-31 23:18:16 +0300

    stage4test2

--- src/Cli.php
+++ src/Cli.php
@@ -8,6 +8,6 @@ use function cli\prompt;
 function greeting()
 {
     line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
+    $name = prompt('May I have your name?');qs
     line("Hello, %s!", $name);
 }
