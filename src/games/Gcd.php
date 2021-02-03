31877c879748d3850a7eda577b5be8080e4fe5b0
1NQ457 <alex.kot14@gmail.com>
2021-02-03 07:57:53 +0300

    fixes pt.1

--- src/games/Gcd.php
+++ src/games/Gcd.php
@@ -6,13 +6,22 @@ use function Brain\Engine\play;
 
 const GAME_DEF = 'Find the greatest common divisor of given numbers.';
 
+function gcd($num1, $num2)
+{
+    if ($num2 == 0) {
+        return $num1;
+    }
+
+    return gcd($num2, $num1 % $num2);
+}
+
 function run()
 {
     $logic = function () {
         $num1 = rand(1, 99);
         $num2 = rand(1, 99);
         $question = "{$num1} {$num2}";
-        $correctAnswer = gmp_gcd($num1, $num2);
+        $correctAnswer = gcd($num1, $num2);
 
         return [$question, $correctAnswer];
     };
--- src/games/Prime.php
+++ src/games/Prime.php
@@ -6,12 +6,22 @@ use function Brain\Engine\play;
 
 const GAME_DEF = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+function isPrime($num)
+{
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if (!($num % $i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function run()
 {
     $logic = function () {
         $num = rand(1, 99);
         $question = "{$num}";
-        $correctAnswer = (gmp_prob_prime($num) == 2) ? 'yes' : 'no';
+        $correctAnswer = (isPrime($num)) ? 'yes' : 'no';
 
         return [$question, $correctAnswer];
     };
